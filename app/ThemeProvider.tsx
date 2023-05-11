1d3a5962b42cb54c61a6626345ebe6eeb66d2d79
dave-woods <>
2023-05-11 12:56:26 +0100

    Maybe a way to persist theme...

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
  
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
  
 export const ThemeContext = createContext<{theme: string, setTheme: Function}>({theme: '', setTheme: () => ''});
  
@@ -8,5 +8,13 @@ export default function ThemeProvider({ children }: {
     children: React.ReactNode
   }) {
     const [theme, setTheme] = useState('dark')
+    useEffect(() => {
+        if (localStorage.theme) {
+            setTheme(localStorage.theme)
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.theme = theme;
+    }, [theme])
     return <ThemeContext.Provider value={{theme, setTheme}}>{children}</ThemeContext.Provider>;
 }
